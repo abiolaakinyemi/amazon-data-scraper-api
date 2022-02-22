96cc14ea3379829dd9caa4ea320053bf56d8f814
abiolaakinyemi <84160458+abiolaakinyemi@users.noreply.github.com>
2022-02-21 21:05:18 -0500

    first commit
    I add the first endpoint to get the product detail from Amazon.

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const apiKey = "927a78b01b4c775e541f5a5a6250362e";
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon hey hey");
+});
+
+// Get Product Details
+
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/${productId}`
+    );
+
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
